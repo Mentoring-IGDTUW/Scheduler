5e23ae23ec531bc7a48c748129889025ec476201
Nikshubha <nikshubhakumar@gmail.com>
2017-10-06 22:26:44 +0530

    Updated models

--- main/sch/models.py
+++ main/sch/models.py
@@ -4,21 +4,24 @@ from django.utils import timezone
 class Schedule(models.Model):
 	day = models.CharField(max_length=8)
 	slots = models.CharField(max_length=8)
-	#def __str__(self):
-    #	return self.slots
+	
+	def __str__(self):
+		return self.slots
 
 class Person(models.Model):
 	name = models.CharField(max_length=40)
 	subject = models.CharField(max_length=6)
 	TT = models.OneToOneField(Schedule)
-	#def __str__(self):
-    #	return self.name
+	
+	def __str__(self):
+		return self.name
 
 class Batch:
 	stream = models.CharField(max_length=4)
 	year = models.IntegerField(blank=False)
 	TT = models.OneToOneField(Schedule)
-	#def __str__(self):
-    #	return self.stream
+	
+	def __str__(self):
+		return self.stream
 
 
