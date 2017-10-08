abb173af81adbfab8a0e53ec97e120c7cd2efe0b
Nikshubha <nikshubhakumar@gmail.com>
2017-10-09 01:37:07 +0530

    Admin made

--- main/sch/admin.py
+++ main/sch/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Schedule
+from .models import Person
+from .models import Batch
 
+admin.site.register(Schedule)
+admin.site.register(Person)
+admin.site.register(Batch)
 # Register your models here.
