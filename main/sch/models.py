53e4a6ccace454988ecc05c5bc403318c48cd412
Nikshubha <nikshubhakumar@gmail.com>
2017-10-21 22:33:56 +0530

    Changed Models

--- main/sch/models.py
+++ main/sch/models.py
@@ -1,29 +1,35 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User, UserManager, Group
 
 class Schedule(models.Model):
-	day = models.CharField(max_length=8)
-	slots = models.CharField(max_length=8)
+	day = models.CharField(max_length=8,default="")
+	slots = models.CharField(max_length=8,default="")
 	
 	def __str__(self):
 		return self.slots
 
-class Person(models.Model):
-	Tid =models.CharField(max_length=6, default="", editable=False)
-	password =models.CharField(max_length=10, default="")
-	name = models.CharField(max_length=40)
-	subject = models.CharField(max_length=6)
-	TT = models.OneToOneField(Schedule)
-	
-	def __str__(self):
-		return self.name
-
 class Batch(models.Model):
-	stream = models.CharField(max_length=4)
-	year = models.IntegerField(blank=False)
+	group=models.OneToOneField(Group,null=True)
+	stream = models.CharField(max_length=4,default="")
+	year = models.IntegerField(blank=False,default=0)
 	TT = models.OneToOneField(Schedule)
 	
 	def __str__(self):
 		return self.stream
+
+class Person(User):
+	Tid =models.CharField(max_length=6, default="", editable=False)
+	#password =models.CharField(max_length=10, default="")
+	#name = models.CharField(max_length=40)
+	subject = models.CharField(max_length=6,default="")
+	batch=models.ForeignKey(Batch,null=True)
+	TT = models.OneToOneField(Schedule)
+	objects=UserManager()
+	
+	def __str__(self):
+		return self.name
+
+
 
 
