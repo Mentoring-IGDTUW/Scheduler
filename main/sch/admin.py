84368a573ad89584a96834436c817b51bf002fcc
Srishti-Bhatnagar <srishtibhatnagar06@gmail.com>
2017-10-23 22:59:56 +0530

    models modified

--- main/sch/admin.py
+++ main/sch/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Schedule
 from .models import Person
 from .models import Batch
+from .models import KeyVal
+from .models import Dicty
 
 admin.site.register(Schedule)
 admin.site.register(Person)
 admin.site.register(Batch)
+admin.site.register(Dicty)
+admin.site.register(KeyVal)
 # Register your models here.
--- main/sch/models.py
+++ main/sch/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User, UserManager, Group
 
+class Dicty(models.Model):
+	name = models.CharField(max_length=50,default="")
+
+
+	def __str__(self):
+		return self.name
+
+class KeyVal(models.Model):
+	container = models.OneToOneField(Dicty,db_index=True)
+	key = models.IntegerField(blank=False,default=0,db_index=True)
+	value = models.CharField(max_length=4,default="",db_index=True)
+
+
+	def __str__(self):
+		return self.value
+		
+
+
 class Schedule(models.Model):
 	day = models.CharField(max_length=8,default="")
 	slots = models.CharField(max_length=8,default="")
@@ -14,6 +32,8 @@ class Batch(models.Model):
 	stream = models.CharField(max_length=4,default="")
 	year = models.IntegerField(blank=False,default=0)
 	TT = models.OneToOneField(Schedule)
+	dic = models.ManyToManyField(KeyVal,null=True)
+
 	
 	def __str__(self):
 		return self.stream
@@ -26,7 +46,9 @@ class Person(User):
 	batch=models.ForeignKey(Batch,null=True)
 	TT = models.OneToOneField(Schedule)
 	objects=UserManager()
-	
+	dic = models.OneToOneField(KeyVal,null=True)
+
+
 	def __str__(self):
 		return self.name
 
